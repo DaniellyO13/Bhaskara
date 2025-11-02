e29d1c680b45f98ae9a5c399622fa7e01af15dcf
Danielly Oliveira <danyoliveiraa001@gmail.com>
2025-11-02 18:22:53 -0300

    Conclusão

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿//Criar a formula de bhaskara
+
+double a, b, c, delta, x1, x2;
+
+Console.WriteLine("--- Equação de segundo grau ---");
+
+Console.Write("(A) = ");
+a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("(B) = ");
+b = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("(C) = ");
+c = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine();
+
+if (a == 0)
+{
+    Console.WriteLine("Não é uma equação de segundo grau!");
+}
+else
+{
+    delta = b * b - 4 * a * c;
+
+    if (delta < 0)
+    {
+
+        Console.WriteLine($"Como delta = {delta:N2}, a equação não possui raízes reais!");
+
+    }
+    else
+    {
+        x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+        x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+        Console.WriteLine($"x1 = {x1:N2} e x2 = {x2:N2}");
+    }
+
+}
